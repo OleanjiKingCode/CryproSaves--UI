f884ce9f1f5e1d05bc11005ac5b0b12889d2f6f9
OleanjiKingCode <holamilean56@gmail.com>
2024-02-15 07:32:28 +0100

    completed dialog

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,7 +1,6 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogOverlay,
@@ -11,36 +10,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogPortal,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "./ui/alert-dialog";
-// import * as Dialog from '@radix-ui/react-dialog';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { DialogClose } from "@radix-ui/react-dialog";
-import { RiCloseLine } from "react-icons/ri";
+import { FaLock } from "react-icons/fa";
 
 export const Create = ({
   isOpen,
@@ -56,32 +26,45 @@ export const Create = ({
         <DialogHeader>
           <DialogTitle>Create Save</DialogTitle>
         </DialogHeader>
-        <div className="grid gap-5 py-4">
-          <div className="grid grid-cols-5 items-center gap-4">
-            <Label htmlFor="name">Name</Label>
-            <Input
-              id="name"
-              placeholder="Oleanji"
-              className="col-span-2 border-[2px] border-gray-500"
-            />
+        <div className="">
+          <div className="grid grid-cols-5 gap-5 py-4">
+            <div className="grid col-span-3 ">
+              <div className="grid grid-cols-3 gap-4 col-span-3 items-center">
+                <Label htmlFor="name" className="col-span-1">
+                  Name
+                </Label>
+                <Input
+                  id="name"
+                  placeholder="Oleanji"
+                  className="border-[2px] border-gray-500 col-span-2 focus-visible:ring-0"
+                />
+              </div>
+              <div className="grid grid-cols-3 gap-4 col-span-3 items-center">
+                <Label htmlFor="days" className="col-span-1">
+                  Lock Days
+                </Label>
+                <Input
+                  id="days"
+                  type="number"
+                  placeholder="300"
+                  className="col-span-2 border-[2px] border-gray-500 outline-none focus-visible:ring-0"
+                />
+              </div>
+            </div>
+
+            <div className="bg-green-200 rounded-md items-center w-full flex justify-center shadow-md py-10 col-span-2 ">
+              <FaLock className="mr-2 h-14 w-14 text-green-600 " />
+            </div>
           </div>
+
           <div className="grid grid-cols-5 items-center gap-4">
             <Label htmlFor="username">Amount</Label>
             <Input
               id="username"
               placeholder="10.01"
-              className="col-span-3 border-[2px] border-gray-500"
+              className="col-span-3 border-[2px] border-gray-500 focus-visible:ring-0"
             />
             <Button className="bg-blue-400 font-medium px-7">MAX</Button>
-          </div>
-          <div className="grid grid-cols-5 items-center gap-4">
-            <Label htmlFor="days">Lock Days</Label>
-            <Input
-              id="days"
-              type="number"
-              placeholder="300"
-              className="col-span-3 border-[2px] border-gray-500 outline-none focus:outline-none"
-            />
           </div>
         </div>
         <DialogFooter className="flex flex-row items-center " onClick={onClose}>
