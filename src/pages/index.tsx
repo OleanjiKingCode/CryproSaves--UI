be2e228595d1785a50001d1d65d018dca8f92f20
OleanjiKingCode <holamilean56@gmail.com>
2024-03-04 10:07:10 +0100

    feat: main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import Main from '@/components/Main';
-import { Button } from '@/components/ui/button';
 import { Toaster } from '@/components/ui/toaster';
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col min-h-screen  bg-pink-100">
-      {/* <Main /> */}
-     
-
+      <Main />
       <Toaster />
     </div>
   );
