232eabee61ca118dd9e1942d9e9a50298fedcf13
OleanjiKingCode <holamilean56@gmail.com>
2024-03-05 18:53:55 +0100

    feat: homepage added

--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Button } from '../ui/button';
 
-export default function home() {
+export default function HomePage() {
   return (
-    <div className="w-full flex flex-col gap-5 items-center justify-center">
-      <h2 className="text-xl bg-white p-5 text-pink-400 font-semibold">
-        Crypto Saves
-      </h2>
-      <span className="text-white italic font-medium">
-        personal savings dapp
-      </span>
-      <div>
-        <Button>Login</Button>
-        <Button>Create Account</Button>
+    <div className="w-full flex flex-col min-h-screen gap-5 items-center justify-center px-20">
+      <div className="w-[50rem] bg-white px-5 py-20 text-center rounded-2xl">
+        <h2 className="text-2xl w-full bg-white text-pink-400 text-center font-semibold rounded-lg">
+          Crypto Saves
+        </h2>
+        <span className="text-black italic font-medium">
+          your personal crypto savings dapp
+        </span>
+        <div className="w-full flex flex-col items-center pt-10 justify-center gap-5">
+          <Button className="w-[25rem]">Login</Button>
+          <Button className="w-[25rem]">Create Account</Button>
+        </div>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+import HomePage from '@/components/Home/home';
 import Main from '@/components/Main';
 import { Toaster } from '@/components/ui/toaster';
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col min-h-screen  bg-pink-100">
-      <Main />
+      {/* <Main /> */}
+      <HomePage />
       <Toaster />
     </div>
   );
